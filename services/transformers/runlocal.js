de933eff07fdcf83e66463fa385db1032dfc4bd2
Anoop Kumar <anoop_kr@yahoo.com>
2019-03-11 15:57:28 +0530

    Test added to transformera

--- services/transformers/runlocal.js
+++ services/transformers/runlocal.js
@@ -1,7 +1,14 @@
 const event = require('./event.json')
 const transformer = require('./sd')
+const assert = require("assert")
 
 transformer.handler(event.transform, {}).then(function(store, error){
-	const fs = require('fs');
-	fs.writeFileSync("./snapshots.json", JSON.stringify(store));	
+	const fs = require('fs');	
+	try{
+		assert.equal(store.abs.metadata.organization, "Maruti");
+	}
+	catch(err){
+		console.log(err);
+	}
+	fs.writeFileSync("./snapshots.json", JSON.stringify(store));		
 })
