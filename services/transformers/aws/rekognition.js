cf41385790028b15e871b21c3da6e6b37b263f2b
anoopk <anoop_kr@yahoo.com>
2019-05-20 23:00:20 +0530

    AWS using the common images folder

--- services/transformers/aws/rekognition.js
+++ services/transformers/aws/rekognition.js
@@ -11,7 +11,7 @@ AWS.config.update({region:'us-east-1'});
 function getImageMetadata(){
   return new Promise((resolve, reject)=>{
 
-    const imagesPath = path.join(__dirname, "../aws/Images/" + entity);
+    const imagesPath = path.join(__dirname, "../Images/" + entity);
     console.log(`Reading images from ${imagesPath}`);
 
     fs.readdir(imagesPath, function(err, items) {  
